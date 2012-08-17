a89d4b54d8acbdf6a1d6e82a15d4d69dbd33f519
Keith Stenson <kstenson@gmail.com>
2012-08-17 16:12:06 +0100

    inital commit

---
+++ NCalcRnD/Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NCalc;
+
+namespace NCalcRnD.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var Types = AppDomain.CurrentDomain.GetAssemblies().ToList()
+                .SelectMany(s => s.GetTypes())
+                .Where(p => typeof (IEntity).IsAssignableFrom(p))
+                .Select(type => new EntityDef()
+                {
+                    TypeName = type.Name,
+                    Properties =type.GetProperties().Select(x => type.Name+"." + x.Name).ToList()
+                }).ToList();
+   
+                                         
+                                 
+            return View(Types);
+        }
+
+        public ActionResult Calculate(string calculation)
+        {
+            dynamic response = new CalculationResult();
+            Expression expression = new Expression(calculation);
+            if (expression.HasErrors())
+            {
+                response.success = false;
+                response.errors = expression.Error;
+            }
+           
+            try
+            {
+               response.result = expression.Evaluate();
+               response.success = true;
+            }
+            catch (Exception ex)
+            {
+                response.success = false;
+                response.errors = ex.Message;
+            }
+            return Json(response);
+        }
+
+        public class CalculationResult
+        {
+            public object result { get; set; }
+            public bool success { get; set; }
+            public string errors { get; set; }
+        }
+
+        public class Bill : IEntity
+        {
+            public DateTime StartDate { get; set; }
+            public DateTime EndDate { get; set; }
+            public decimal NetAmount { get; set; }
+            public int Quantity { get; set; }
+        }
+
+        public class Contract :IEntity
+        {
+            public int MIC { get; set; }
+        }
+    }
+
+    public interface IEntity
+    {
+    }
+
+    public class EntityDef
+    {
+        public string TypeName { get; set; }
+        public List<string> Properties { get; set; }
+
+        public EntityDef()
+        {
+            Properties = new List<string>();
+        }
+    }
+}
---
+++ NCalcRnD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NCalcRnD.MvcApplication" Language="C#" %>
